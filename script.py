b9c19d9f9a2558b0383c8824d960e4ed85abf35d
Vedant <76871277+theofficialvedantjoshi@users.noreply.github.com>
2023-11-02 17:47:27 +0530

    fixed progress bar

--- script.py
+++ script.py
@@ -9,6 +9,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from PIL import Image
+import tkinter as tk
+from tkinter import ttk
+import threading
 
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -24,73 +27,76 @@ entry2 = None
 entry3 = None
 pg = None
 def action():
-    global entry1
-    global entry2
-    global entry3
-    global pg
-    global root
-    df = pd.DataFrame(columns=['Amount','Date'])
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    def process_emails():
+        global entry1
+        global entry2
+        global entry3
+        global pg
+        global root
+        df = pd.DataFrame(columns=['Amount','Date'])
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-    try:
-        # Call the Gmail API
-        amounts = []
-        dates = []
-        bnk = []
-        bank = entry1.get()
-        service = build('gmail', 'v1', credentials=creds)
-        l_list = service.users().messages().list(userId='me',q=("from:{} is:unread".format(bank))).execute()
-        ids=[]  
-        for i in l_list.get('messages'):
-            ids.append(i.get('id'))
-        n = int(entry2.get())
-        stepval = 100/n
-        #getting message ids and email bodies and adding that to our lists.
-        for i in range(n):
-            request = service.users().messages().get(userId='me',id=ids[i],)
-            request.execute()
-            transaction = str(request.execute().get('snippet'))
-            #print("Computing transaction ",i+1," of ",n,'...\n')
-            try:
-                amounts.append(float(re.findall(r'Rs.\d+',transaction)[0].strip('Rs.')))
-                dates.append(re.findall(r'\d{2}[-]\d{2}[-]\d{2}',transaction)[0])
-            except:
-                continue
-            pg.set(pg.get()+stepval)
-        path = entry3.get() + "\RECORDS.xlsx"
-        #creating dataframe.
-        df['Amount'] = amounts
-        df['Date'] = dates
-        df.set_index('Date', inplace=True)
-        inf = df.describe()
-        daywise = df.groupby('Date').sum()
-        #writing to excel file.
-        with pd.ExcelWriter(path) as writer:
-            df.to_excel(writer, sheet_name="BANK RECORDS", index=True)
-            daywise.to_excel(writer, sheet_name="DAYWISE", index=True)
-            inf.to_excel(writer, sheet_name="STATISTICS", index=True)
-        #print("Records computed.")
-        
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
-
+        try:
+            # Call the Gmail API
+            amounts = []
+            dates = []
+            bnk = []
+            bank = entry1.get()
+            service = build('gmail', 'v1', credentials=creds)
+            l_list = service.users().messages().list(userId='me',q=("from:{} is:unread".format(bank))).execute()
+            ids=[]  
+            for i in l_list.get('messages'):
+                ids.append(i.get('id'))
+            n = int(entry2.get())
+            stepval = 100/n
+            pg['maximum'] = 100
+            #getting message ids and email bodies and adding that to our lists.
+            for i in range(n):
+                request = service.users().messages().get(userId='me',id=ids[i],)
+                request.execute()
+                transaction = str(request.execute().get('snippet'))
+                #print("Computing transaction ",i+1," of ",n,'...\n')
+                try:
+                    amounts.append(float(re.findall(r'Rs.\d+',transaction)[0].strip('Rs.')))
+                    dates.append(re.findall(r'\d{2}[-]\d{2}[-]\d{2}',transaction)[0])
+                except:
+                    continue
+                pg['value']+=stepval
+            path = entry3.get() + "\RECORDS.xlsx"
+            #creating dataframe.
+            df['Amount'] = amounts
+            df['Date'] = dates
+            df.set_index('Date', inplace=True)
+            inf = df.describe()
+            daywise = df.groupby('Date').sum()
+            #writing to excel file.
+            with pd.ExcelWriter(path) as writer:
+                df.to_excel(writer, sheet_name="BANK RECORDS", index=True)
+                daywise.to_excel(writer, sheet_name="DAYWISE", index=True)
+                inf.to_excel(writer, sheet_name="STATISTICS", index=True)
+            #print("Records computed.")
+            
+        except HttpError as error:
+            # TODO(developer) - Handle errors from gmail API.
+            print(f'An error occurred: {error}')
+    email_processing_thread = threading.Thread(target=process_emails)
+    email_processing_thread.start()
 
 
 def main():
@@ -146,10 +152,12 @@ def main():
     button = customtkinter.CTkButton(frame, text="Submit", font=("Roboto", 12), command=action)
     button.pack(pady=12, padx=10)
 
-    pg = customtkinter.CTkProgressBar(frame,orientation="horizontal", mode="determinate",border_color="white")
+    style = ttk.Style()
+    style.configure("TProgressbar", background="black")
+    
+    pg = ttk.Progressbar(frame,orient="horizontal", mode="determinate", length=100)
     pg.pack(pady=12, padx=10)
-    pg.set(0)
-
+    
     label = customtkinter.CTkLabel(frame, text="YOU WILL BE REDIRECTED TO SIGN-IN", font=("Roboto", 12))
     label.pack(pady=12, padx=10)
 
